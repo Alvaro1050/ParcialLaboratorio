dde5370c0ad2dc6a512aa60449a2d78596835273
Daryl110 <daryl.8220@gmail.com>
2017-05-22 10:17:58 -0500

    numeroMenor

--- ParcialLaboratorio/src/parciallaboratorio/ParcialLaboratorio.java
+++ ParcialLaboratorio/src/parciallaboratorio/ParcialLaboratorio.java
@@ -23,41 +23,44 @@ public class ParcialLaboratorio {
         int numero2 = Integer.parseInt(JOptionPane.showInputDialog("ingrese el Segundo numero"));
         int numero3 = Integer.parseInt(JOptionPane.showInputDialog("ingrese el Tercer numero"));
         
-        int numeroMayor = 0;
+        int numeroMenor = 0;
 
         if (numero1 > numero2 && numero1 > numero3) {
             System.out.println(numero1);
-            numeroMayor = numero1;
             if (numero2 > numero3) {
                 System.out.println(numero2);
                 System.out.println(numero3);
+                numeroMenor = numero3;
             } else {
                 System.out.println(numero3);
                 System.out.println(numero2);
+                numeroMenor = numero2;
             }
         } else if (numero2 > numero1 && numero2 > numero3) {
             System.out.println(numero2);
-            numeroMayor = numero2;
             if (numero1 > numero3) {
                 System.out.println(numero1);
                 System.out.println(numero3);
+                numeroMenor = numero3;
             } else {
                 System.out.println(numero3);
                 System.out.println(numero1);
+                numeroMenor = numero1;
             }
         } else if (numero3 > numero1 && numero3 > numero2) {
             System.out.println(numero3);
-            numeroMayor = numero3;
-            if (numero2>numero1) {
+            if (numero2 > numero1) {
                 System.out.println(numero2);
                 System.out.println(numero1);
+                numeroMenor = numero1;
             }else{
                 System.out.println(numero1);
                 System.out.println(numero2);
+                numeroMenor = numero2;
             }
         }
         
-        System.out.println("El numero mayor entre ("+numero1+","+numero2+","+numero3+") es: "+numeroMayor);
+        System.out.println("El numero menor entre ("+numero1+","+numero2+","+numero3+") es: "+numeroMenor);
     }
 
 }
